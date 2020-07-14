4b5d7e4964ea67f5638372a70c5f9aac17f8daba
Mario Krstevski <mariokrstevski@hotmail.com>
2020-07-14 09:06:21 +0200

    Tasks shown to users on dashboard

--- app/Http/Controllers/User/HomeController.php
+++ app/Http/Controllers/User/HomeController.php
@@ -3,12 +3,25 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Task;
 use Illuminate\Http\Request;
 
+class Test {
+    private  $name;
+    public function __construct($a)
+    {
+
+        $this->name = $a;
+
+    }
+}
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('user.home');
+//        $tasks = array();
+//        $tasks[0] = new Test('ime');
+        $tasks = Task::all();
+        return view('user.home')->with('tasks', $tasks);
     }
 }
--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -2,25 +2,45 @@
 
 @section('content')
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">
-                        Dashboard
-                    </div>
+{{--       You are logged in--}}
+    </div>
+    <div class="container">
+        <h3>Available Tasks</h3>
+        <ul class="list-group">
+            @foreach($tasks as $task)
+                @if($task->active && (\Carbon\Carbon::parse($task->scheduled_at)->timestamp < \Carbon\Carbon::now()->timestamp) && (
+    \Carbon\Carbon::parse($task->deadline)->timestamp > \Carbon\Carbon::now()->timestamp
+)  )
 
-                    <div class="card-body">
-                        @if(session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
+                <div class="list-group-item">
+                   <div class="row">
+                       <div class="col-9">
+                           <h4>
+                               {{$task->name}}
+                           </h4>
+                           <p>Description: {!! $task -> description !!}</p>
+                           <p>Due Date: <span class="text-black-50">{{ \Carbon\Carbon::parse( $task->deadline)->format('d M Y') }}</span> </p>
+                       </div>
+                       <div class="col-3 text-right">
+                           <div>
 
-                        You are logged in!
-                    </div>
+                           <b>$ {{$task -> reward_amount}}</b>
+                           </div>
+                           <div class="flex">
+                               <button class="btn btn-primary" style="margin-top: 35px;">Start</button>
+
+                           </div>
+
+                       </div>
+                   </div>
                 </div>
-            </div>
-        </div>
+                @endif
+
+            @endforeach
+
+
+        </ul>
+
     </div>
 @endsection
 @section('scripts')
